37733c3cb8ccdb466fea80e4ee7c66fba16d7553
Junn34 <71059080+junn34@users.noreply.github.com>
2024-11-26 21:06:16 +0900

    Update client.c

--- client.c
+++ client.c
@@ -5,17 +5,7 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 
-#define SOCKET_PATH "banking_socket"
-#define MAX_NAME_LEN 50
-#define MAX_PASS_LEN 20
-#define BUFFER_SIZE 256
 
-typedef struct {
-    char name[MAX_NAME_LEN];
-    char account_number[20];
-    char password[MAX_PASS_LEN];
-    double balance;
-} Account;
 
 void create_account(int socket_fd);
 void deposit(int socket_fd);
