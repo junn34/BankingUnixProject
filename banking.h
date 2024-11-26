bbbe081ef4742aab0b4f23b971d4039b6ef3810e
Junn34 <71059080+junn34@users.noreply.github.com>
2024-11-26 21:06:36 +0900

    Update banking.h

--- banking.h
+++ banking.h
@@ -1,23 +1,18 @@
 #ifndef BANKING_H
 #define BANKING_H
 
+// 소켓 경로 및 버퍼 크기 정의
 #define SOCKET_PATH "banking_socket"
 #define MAX_NAME_LEN 50
 #define MAX_PASS_LEN 20
 #define BUFFER_SIZE 256
 
+// 계좌 정보를 저장하는 구조체
 typedef struct {
-    char name[MAX_NAME_LEN];
-    char account_number[20];
-    char password[MAX_PASS_LEN];
-    double balance;
+    char name[MAX_NAME_LEN];       // 사용자 이름
+    char account_number[20];       // 계좌 번호
+    char password[MAX_PASS_LEN];   // 계좌 비밀번호
+    double balance;                // 계좌 잔액
 } Account;
-
-typedef struct {
-    char timestamp[30];
-    char operation[10];  // "DEPOSIT", "WITHDRAW", etc.
-    double amount;
-    double balance_after;
-} Transaction;
 
 #endif
